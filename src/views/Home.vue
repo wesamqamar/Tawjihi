71f3f74d839e9b14361e3bc76ddcff750c6f64a0
sliemansalah <slieman2001@hotmail.com>
2022-03-04 20:10:25 +0200

    up..

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div>
-      <div class="logo m-4">
-        <img width="60" height="60" src="/img/logo.png" alt="">
-    <h1 class="d-inline-block mx-2 primaryFont text-uppercase logoText primaryColor">tawjihi</h1>
-      </div>
+    <b-container>
+      <b-navbar toggleable="lg">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-button pill class="bg-blue ms-5 loginBtn">تسجيل الدخول</b-button>
+          <b-navbar-nav class="ml-auto primaryColor fw-bold">
+            <b-nav-item class="link" href="#">المسارات التعليمية</b-nav-item>
+            <b-nav-item class="link" href="#">عن الموقع</b-nav-item>
+            <b-nav-item class="link" href="#">اتصل بنا</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+
+        <b-navbar-brand class="logo" href="#">
+          <h3
+            class="
+              d-inline-block
+              mx-2
+              logoFont
+              text-uppercase
+              logoText
+              secondaryColor
+            "
+          >
+            tawjihi
+          </h3>
+          <img width="50" height="50" src="/img/logo.png" alt="" />
+        </b-navbar-brand>
+      </b-navbar>
+    </b-container>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
