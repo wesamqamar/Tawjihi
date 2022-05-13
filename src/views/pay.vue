b0513af366b54713b672ea75d16cb5cb0e4ec871
sliemansalah <slieman2001@hotmail.com>
2022-05-13 20:51:23 +0300

    up..

--- src/views/pay.vue
+++ src/views/pay.vue
@@ -11,66 +11,61 @@
         <b-col md="7">
           <div class="content">
             <h2 class="px-3 py-2">طريقة الدفع</h2>
-
-            <h5>رقم البطاقة</h5>
-
-            <b-form-input  v-model="email"></b-form-input>
+            
             <b-row>
-              <b-col md="6">
-                <div>
-                  <h5>تاريخ انتهاء الصلاحية</h5>
-                </div>
+              <b-col md="12">
+                <label style="position:relative;top:10px;">رقم البطاقة</label>
+                <b-form-input class="customInputStyle" v-model="inputs.card_number"></b-form-input>
+                <img style="width:40px;height:40px;float:left;position:relative;bottom:50px;left:10px" src="/img/visa.png" alt="">
               </b-col>
-              <b-col md="6">
-                <div>
-                  <h5>رمز البطاقة</h5>
-                </div></b-col
-              >
-            </b-row>
-            <b-row>
-              <b-col md="6">
-                <div>
-                  <b-form-input v-model="text"></b-form-input>
-                </div>
-              </b-col>
-              <b-col md="6">
-                <div>
-                  <b-form-input v-model="text1"></b-form-input></div
-              ></b-col>
-            </b-row>
 
-            <b-row>
-              <b-col md="6">
-                <div>
-                  <h5>الاسم الاول</h5>
-                </div>
+               <b-col md="6">
+                <label>تاريخ انتهاء الصلاحية</label>
+                <b-form-datepicker placeholder="" class="customInputStyle" v-model="inputs.expired_date"></b-form-datepicker>
               </b-col>
-              <b-col md="6">
-                <div>
-                  <h5>الاسم الاخير</h5>
-                </div></b-col
-              >
-            </b-row>
-            <b-row>
-              <b-col md="6">
-                <div>
-                  <b-form-input v-model="text"></b-form-input>
-                </div>
-              </b-col>
-              <b-col md="6">
-                <div>
-                  <b-form-input v-model="text1"></b-form-input></div
-              ></b-col>
-            </b-row>
 
+              <b-col md="6">
+                <label>رمز البطاقة</label>
+                <b-form-input class="customInputStyle" v-model="inputs.card_code"></b-form-input>
+              </b-col>
+
+               <b-col md="6">
+                <label>الاسم الأول</label>
+                <b-form-input class="customInputStyle" v-model="inputs.first_name"></b-form-input>
+              </b-col>
+
+              <b-col md="6">
+                <label>الاسم الأخير</label>
+                <b-form-input class="customInputStyle" v-model="inputs.last_name"></b-form-input>
+              </b-col>
+
+
+            </b-row>
+    
             <div class="actionsBtn">
               <b-button pill class="exploreBtn" v-b-modal.payModal>
                 ادفع الان
               </b-button>
-              <b-modal id="payModal" size="md" :hide-footer="true" :hide-header="true">
+              <b-modal
+                id="payModal"
+                size="md"
+                :hide-footer="true"
+                :hide-header="true"
+              >
                 <div class="text-center">
-                  <img width="100px " height="100px"  src="img/mp.png" alt="">
-                  <h2>مرحبا بك</h2>
+                  <img
+                    style="position: relative; bottom: 70px"
+                    width="100px "
+                    height="100px"
+                    src="img/mp.png"
+                    alt=""
+                  />
+                </div>
+                <div
+                  class="text-center"
+                  style="position: relative; bottom: 60px"
+                >
+                  <h2>مرحبا بك !</h2>
 
                   <p>يمكنك البدء بعملية الدفع على الفور</p>
 
@@ -128,6 +123,23 @@
                         >ملفك الشخصي</span
                       >
                     </p>
+
+                    <div>
+                      <b-button
+                        style="
+                          float: left;
+                          margin-bottom: -50px;
+                          margin-left: 40px;
+                          width: auto;
+                          min-width: 140px;
+                          background: #20577a;
+                          border: 1px solid #20577a;
+                        "
+                        pill
+                      >
+                        استكشف الآن
+                      </b-button>
+                    </div>
                   </div>
                 </div>
               </b-modal>
@@ -147,6 +159,13 @@ export default {
   },
   data() {
     return {
+      inputs: {
+        card_number: null,
+        expired_date: null,
+        card_code: null,
+        first_name: null,
+        last_name: null,
+      },
       text: "",
       text1: "",
       email: "",
@@ -156,14 +175,16 @@ export default {
   },
   methods: {
     goToPage(page) {
-      this.$router.push({name: page})
-    }
-  }
+      this.$router.push({ name: page });
+    },
+  },
 };
 </script>
 
 <style>
 .modal-content {
   border-radius: 5.3rem;
+  position: relative;
+  top: 100px;
 }
 </style>
